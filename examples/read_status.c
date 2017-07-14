fc4ec65c8c1b4deafedd2515202129f93de17a91
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-07-14 00:57:13 -0500

    add read status example

---
+++ examples/read_status.c
@@ -0,0 +1,37 @@
+#include "nuphasedaq.h" 
+#include <stdio.h> 
+#include <inttypes.h>
+#include <stdlib.h> 
+
+
+// read_status spidev
+int main(int nargs, char ** args) 
+{
+  int not_ok;
+  nuphase_dev_t * dev;
+  nuphase_status_t status; 
+
+
+  if (nargs < 2) 
+  {
+    printf("Usage: read_status spidev\n"); 
+    return 1; 
+  }
+
+  //no interrupt, no locking, default config, 
+  dev = nuphase_open(args[1],0,0,0); 
+  not_ok = nuphase_read_status(dev, &status); 
+  nuphase_close(dev); 
+
+  if (not_ok)
+  {
+    printf("Something went wrong :(\n"); 
+  }
+  else
+  {
+    nuphase_print_status(stdout, &status); 
+  }
+
+  return 0; 
+
+}
