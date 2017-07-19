f9fc91609d49e5addeebede3c95257c3fdc65de2
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-07-19 15:33:31 +0000

    add read register

---
+++ examples/read_register.c
@@ -0,0 +1,34 @@
+#include "nuphasedaq.h" 
+
+#include <stdio.h>
+#include <assert.h>
+#include <sys/time.h> 
+#include <stdlib.h>
+
+// read_register spidev  reg
+
+int main(int nargs, char ** args )
+{
+
+  int reg = 7; 
+  if (nargs < 2) 
+  {
+    printf("Usage: read_register spidev [reg=7]\n"); 
+    return 1; 
+  }
+
+  if (nargs > 2) 
+    reg = atoi(args[2]); 
+
+
+  nuphase_dev_t * dev =  nuphase_open(args[1],0,0,0); //no interrupt for now and no threadlocking
+
+  printf("Sending trigger\n"); 
+  nuphase_sw_trigger(dev); 
+  uint8_t result[4]; 
+  nuphase_read_register(dev,reg,result); 
+  printf("READ REGISTER %d, GOT: 0x%x 0x%x 0x%x 0x%x\n",reg, result[0], result[1], result[2], result[3]); 
+  nuphase_close(dev); 
+
+  return 0; 
+}
