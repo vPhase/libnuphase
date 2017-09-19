33dda7e948afebf6860fb88c31b386a335a5db78
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-09-19 17:44:59 +0000

    changes to hk scripts

---
+++ examples/turn_off_boards.c
@@ -0,0 +1,37 @@
+#include "nuphasehk.h" 
+#include <stdio.h> 
+#include <stdlib.h>
+
+//turn_on_boards [http = 0] 
+int main(int nargs, char ** args) 
+{
+  nuphase_hk_t hk; 
+  nuphase_asps_method_t method= nargs < 2 || !atoi(args[1]) ? NP_ASPS_SERIAL : NP_ASPS_HTTP; 
+
+  nuphase_hk(&hk, method); 
+  printf("Before Turn off: \n"); 
+  nuphase_hk_print(stdout,&hk); 
+
+  nuphase_gpio_power_state_t gpio = 0; ; 
+  nuphase_set_gpio_power_state(gpio); 
+
+  printf("after gpio change: \n"); 
+  sleep(1); 
+  nuphase_hk(&hk, method); 
+  nuphase_hk_print(stdout,&hk); 
+
+
+
+  printf("%x\n", hk.on_state); 
+  nuphase_asps_power_state_t asps =  hk.on_state & ~(NP_POWER_MASTER | NP_POWER_SLAVE); 
+  printf("%x\n", asps); 
+  nuphase_set_asps_power_state(asps, method); 
+  sleep(1); 
+  printf("After turn off: \n"); 
+  nuphase_hk(&hk, method); 
+  nuphase_hk_print(stdout,&hk); 
+
+  return 0; 
+
+
+}
